80b1717f5049064c52e133b0030df81e2eb49e25
chwinnie <winnie.r.cheng@gmail.com>
2015-04-28 17:13:23 -0400

    fixed env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 # CLIENT_SECRET = 'CCpN75HD03kAUlGO2kYdgNb9'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
   scope: ['tasks',
     'https://www.googleapis.com/auth/tasks'],
   access_type: 'offline',
