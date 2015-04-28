f62524a32e173fde819ccc89508f53e767e3c81e
chwinnie <winnie.r.cheng@gmail.com>
2015-04-28 19:11:24 -0400

    got authentication to work

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,7 @@
-# CLIENT_ID = '493185566856-73qtkagjj4339kioq4pbc8914qk6sl02.apps.googleusercontent.com'
-# CLIENT_SECRET = 'CCpN75HD03kAUlGO2kYdgNb9'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.GOOGLE_CLIENT_ID, Rails.application.secrets.GOOGLE_CLIENT_SECRET, {
-  scope: ['tasks',
-    'https://www.googleapis.com/auth/tasks'],
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+  scope: ['email',
+  	'https://www.googleapis.com/auth/tasks'],
   access_type: 'offline',
   prompt: 'consent'}
 end
