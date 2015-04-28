9bb261cec6848ec90a5a5268eb0420485e3aa3ab
chwinnie <winnie.r.cheng@gmail.com>
2015-04-28 17:03:02 -0400

    client id and client secret env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,10 @@
-CLIENT_ID = '493185566856-73qtkagjj4339kioq4pbc8914qk6sl02.apps.googleusercontent.com'
-CLIENT_SECRET = 'CCpN75HD03kAUlGO2kYdgNb9'
+# CLIENT_ID = '493185566856-73qtkagjj4339kioq4pbc8914qk6sl02.apps.googleusercontent.com'
+# CLIENT_SECRET = 'CCpN75HD03kAUlGO2kYdgNb9'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, CLIENT_ID, CLIENT_SECRET, {
-  scope: ['email',
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+  scope: ['tasks',
     'https://www.googleapis.com/auth/tasks'],
-    access_type: 'offline'}
+  access_type: 'offline',
+  prompt: 'consent'}
 end
